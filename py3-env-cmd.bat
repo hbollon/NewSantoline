6e036da8e8a5fc9b66d4319a37b45f7e554fef4d
hbollon <hugo.bollon@gmail.com>
2020-10-15 18:56:55 +0200

    Added env config bat files

---
+++ py3-env-cmd.bat
@@ -0,0 +1,21 @@
+set OSGEO4W_ROOT=C:\Program Files\QGIS 3.2
+
+call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
+call "%OSGEO4W_ROOT%\bin\qt5_env.bat"
+call "%OSGEO4W_ROOT%\bin\py3_env.bat"
+
+path %OSGEO4W_ROOT%\apps\qgis\bin;%PATH%
+set GDAL_FILENAME_IS_UTF8=YES
+
+set VSI_CACHE=TRUE
+set VSI_CACHE_SIZE=1000000
+set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
+
+SET PYCHARM="C:\Program Files\JetBrains\PyCharm Community Edition 2020.2.3\bin\pycharm64.exe"
+
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis\python
+set PYTHONHOME=%OSGEO4W_ROOT%\apps\Python36
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\Python36\lib\site-packages;%PYTHONPATH%
+
+set QT_QPA_PLATFORM_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\Qt5\plugins\platforms
+set QGIS_PREFIX_PATH=%OSGEO4W_ROOT%\apps\qgis
