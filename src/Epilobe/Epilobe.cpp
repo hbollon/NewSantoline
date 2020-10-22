601a4e3f00c5fa825d3bd68219e2da309a894fc2
hbollon <hugo.bollon@gmail.com>
2020-10-22 10:44:37 +0200

    Fixed Epilobe and re-compile it

--- src/Epilobe/Epilobe.cpp
+++ src/Epilobe/Epilobe.cpp
@@ -74,7 +74,7 @@ bool Epilobe::subzone() {
 
     std::string parameters = "-overwrite -of gtiff -s_srs "
                       "\"+init=IGNF:LAMB93 +proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 "
-                      "+lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 "
+                      "+lon_0=3 +x_0=700000 +y_0=6600000 +ellps=WGS84 +datum=WGS84 "
                       "+towgs84 = 0, 0, 0, 0, 0, 0, 0 + units = m + no_defs\""
                       " -te "
                       + std::to_string(xmin) + ' '
